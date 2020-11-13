ec5f3723049669d238aaa992135491f42b105c54
CarlosBielma <48254222+CarlosBielma@users.noreply.github.com>
2020-11-13 09:59:08 -0600

    Arreglado grafica de dispersion en su propio js y lista de estados completa

--- scatter.js
+++ scatter.js
@@ -1,4 +1,47 @@
-$(function(){
+$(function () {
     //get the doughnut chart canvas
-    var ctx1 = $("#doughnut-chartcanvas-1");
-
+    var ctx = $("#scatter-chartcanvas-1");
+    var myChart = new Chart(ctx, {
+        type: 'scatter',
+        data: {
+            datasets: [{
+                label: 'Scatter Dataset',
+                data: [{
+                    x: 0,
+                    y: 1
+                }, {
+                    x: 1,
+                    y: 2
+                }, {
+                    x: 2,
+                    y: 3
+                }, {
+                    x: 3,
+                    y: 4
+                }, {
+                    x: 5.4,
+                    y: 6.9
+                }, {
+                    x: 5.6,
+                    y: 7.2
+                }, {
+                    x: 5.8,
+                    y: 7.5
+                }, {
+                    x: 6.1,
+                    y: 8
+                }],
+                borderColor: "green",
+                backgroundColor: "red"
+            }]
+        },
+        options: {
+            scales: {
+                xAxes: [{
+                    type: 'linear',
+                    position: 'bottom'
+                }]
+            }
+        }
+    });
+});
